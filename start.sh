d23f37f2cef9090576dd29545f2cf57826f26a35
sdairs <me@alasdairb.com>
2022-02-13 19:02:37 +0000

    get pg/my db ips and print details to console

--- start.sh
+++ start.sh
@@ -3,3 +3,23 @@ docker-compose -f stack.yml up -d
 git clone https://github.com/airbytehq/airbyte.git
 cd airbyte
 docker-compose up -d
+
+PG_IP=`docker inspect source_pg_db | jq '.[].NetworkSettings.Networks.airbyte_dbt_default.IPAddress' | tr -d '"'`
+MY_IP=`docker inspect dest_my_db | jq '.[].NetworkSettings.Networks.airbyte_dbt_default.IPAddress' | tr -d '"'`
+
+echo "Postgres server details"
+echo "-----------------------"
+echo "Host: ${PG_IP}"
+echo "Port: 5432"
+echo "User: postgres"
+echo "Pass: password"
+echo "-----------------------"
+echo "MariaDB server details"
+echo "-----------------------"
+echo "Host: ${MY_IP}"
+echo "Port: 3306"
+echo "User: root"
+echo "Pass: password"
+echo "-----------------------"
+echo "Visit pgAdmin on http://localhost:8080"
+echo "login with: me@me.me : password"
