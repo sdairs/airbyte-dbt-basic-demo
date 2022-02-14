466e55f39d602ca1e14bbb7dfbd323a7cb4eaa74
sdairs <me@alasdairb.com>
2022-02-14 20:15:37 +0000

    ignore dbt dirs, add profile for using dbt cli

--- get_dbt.sh
+++ get_dbt.sh
@@ -1,3 +1,5 @@
-NORMALIZE_WORKSPACE=`docker run --rm -i -v airbyte_workspace:/data  busybox find /data -path "*normalize/models*" | sed -E "s;/data/([0-9]+/[0-9]+/)normalize/.*;\1;g" | sort | uniq | tail -n 1`
+# NORMALIZE_WORKSPACE=`docker run --rm -i -v airbyte_workspace:/data  busybox find /data -path "*normalize/models*" | sed -E "s;/data/([0-9]+/[0-9]+/)normalize/.*;\1;g" | sort | uniq | tail -n 1`
 
-docker cp airbyte-server:/tmp/workspace/${NORMALIZE_WORKSPACE}/build/run/airbyte_utils/models/generated/ models/
+# docker cp airbyte-server:/tmp/workspace/${NORMALIZE_WORKSPACE}/build/run/airbyte_utils/models/generated/ models/
+
+dbt build --profiles-dir ./.dbt
