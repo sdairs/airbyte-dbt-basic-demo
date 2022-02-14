9ee03599f83d9d8d7f45c36598c174a444bbdfa4
sdairs <me@alasdairb.com>
2022-02-14 21:33:54 +0000

    dbtsh

--- dbt.sh
+++ dbt.sh
@@ -1,5 +1,7 @@
+# Get the workspace for the Basic Normalisation
 # NORMALIZE_WORKSPACE=`docker run --rm -i -v airbyte_workspace:/data  busybox find /data -path "*normalize/models*" | sed -E "s;/data/([0-9]+/[0-9]+/)normalize/.*;\1;g" | sort | uniq | tail -n 1`
-
+# Copy the dbt config
 # docker cp airbyte-server:/tmp/workspace/${NORMALIZE_WORKSPACE}/build/run/airbyte_utils/models/generated/ models/
 
+# Locally build dbt models
 dbt build --profiles-dir ./.dbt
